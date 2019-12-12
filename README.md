ed7fa6c7f01dda2943c6847bf8a4ddb650968974
Giorgio Grassini <giorgiograssini@gmail.com>
2019-12-12 16:56:32 +0100

    Bugfix/empty string (#5)
    * Created tests for the first iteration
    
    * Created tests for the second and third iteration
    
    * Refactor
    
    * Fixed Stop
    
    * Added tests 🧪
    
    * Fixed empty string
    Added prettier
    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ohce-kata
 Solution for Guidesmith's christmas kata by Giorgio &amp; Mario.
+
+Install globally
+```
+npm i -g ohce-kata
+```
+
+To run use
+```
+okta-kata <your name>
+```
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 #! /usr/bin/env node
-const commander = require("commander");
+const commander = require('commander');
 const program = new commander.Command();
-const { greet } = require("./src");
-const { palindrome, isEmpty, listening } = require("./utils");
+const { greet } = require('./src');
+const { palindrome, isEmpty, listening } = require('./utils');
 
-program.version("0.0.1");
+program.version('0.0.1');
 
 program
-.option("<name>")
-.alias("w")
-.description("Word")
-.action(() => {
-	const name = program.args[0];
-	
-	if (!isEmpty(name)) {
-		console.log(greet(name));
-		listening(name);
-	} else {
-		console.log("Insert your name");
-	}
-});
+  .option('<name>')
+  .alias('w')
+  .description('Word')
+  .action(() => {
+    const name = program.args[0];
+
+    if (!isEmpty(name)) {
+      console.log(greet(name));
+      listening(name);
+    } else {
+      console.log('Insert your name');
+    }
+  });
 
 program.parse(process.argv);
